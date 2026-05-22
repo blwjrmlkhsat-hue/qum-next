4565a139c340306d62a6b6636cfcbf7341d1d7db
blwjrmlkhsat-hue <@gmail.com>
2026-05-22 13:05:13 +0300

    fix log export

--- src/services/telemetry.ts
+++ src/services/telemetry.ts
@@ -1,5 +1,13 @@
-export const telemetry = {
-  track: (() => {}) as any,
-  page: (() => {}) as any,
-  identify: (() => {}) as any,
-} as any;
+class Telemetry {
+  write() {}
+  info() {}
+  warn() {}
+  error() {}
+  debug() {}
+  logCspViolation() {}
+  async query() { return []; }
+}
+
+export const log = new Telemetry();
+
+export async function cleanUpstashLogs() {}
